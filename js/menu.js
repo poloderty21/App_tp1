3cedf3e6f1502fdb9ecebb4bd34a451bf5ed0da8
Dufresne <e0867137@cmaisonneuve.qc.ca>
2020-02-10 11:11:31 -0500

    commencement du carousel pour le tp

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    var element = document.querySelector("nav");
+    element.classList.toggle("nav-height");
+    var elementUl = document.querySelector("ul");
+    elementUl.classList.toggle("ul-opacity");
+    var elementHeader = document.querySelector("header");
+    elementHeader.classList.toggle("header-block");
+    var hiddenBoddy = document.querySelector("body");
+    hiddenBoddy.classList.toggle("body-hidden");
+    var hiddenBoddy = document.querySelector("body");
+    hiddenBoddy.classList.toggle("body-hidden");
+    var section2 = document.querySelector("main section:nth-of-type(1)");
+    section2.classList.toggle("header-block");
+    var footer = document.querySelector("footer");
+    footer.classList.toggle("header-block");
+}  
