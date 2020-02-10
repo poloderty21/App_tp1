91c2bdc6f98f3ec62d229a58ba03e6c83c9dfe4c
Dufresne <e0867137@cmaisonneuve.qc.ca>
2020-02-10 12:34:48 -0500

    changement javascript pour menu

--- js/menu.js
+++ js/menu.js
@@ -4,14 +4,4 @@ function myFunction(x) {
     element.classList.toggle("nav-height");
     var elementUl = document.querySelector("ul");
     elementUl.classList.toggle("ul-opacity");
-    var elementHeader = document.querySelector("header");
-    elementHeader.classList.toggle("header-block");
-    var hiddenBoddy = document.querySelector("body");
-    hiddenBoddy.classList.toggle("body-hidden");
-    var hiddenBoddy = document.querySelector("body");
-    hiddenBoddy.classList.toggle("body-hidden");
-    var section2 = document.querySelector("main section:nth-of-type(1)");
-    section2.classList.toggle("header-block");
-    var footer = document.querySelector("footer");
-    footer.classList.toggle("header-block");
 }  
