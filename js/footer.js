916093e54e007bb8478fdc64344e0f84c4ba3817
Dufresne <e0867137@cmaisonneuve.qc.ca>
2020-02-10 14:14:47 -0500

    animation des sections du site

---
+++ js/footer.js
@@ -0,0 +1,18 @@
+$(window).on("load",function(){
+    var screenWith = window.screen.width;
+    if(screenWith>1200){
+        $(window).scroll(function(){
+            var y = $(window).scrollTop();
+            if(y > 150){
+                var array =[];
+                array[0] = document.querySelector(".pleineLargeur h3");
+                array[1] = document.querySelector(".social");
+                array[2] = document.querySelector(".navigation");
+                array[3] = document.querySelector(".copyright");
+                for (let i = 0; i < array.length; i++) {
+                    array[i].classList.add("animationOpaciteSection");
+                }
+            }
+        });
+    }
+});
