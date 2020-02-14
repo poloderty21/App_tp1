e9386492ada2c6b309a03d86131fda9bb2687521
Dufresne <e0867137@cmaisonneuve.qc.ca>
2020-02-14 08:30:00 -0500

    ajout du smooth scrolling en jQuery

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,15 @@ function myFunction(x) {
     var elementUl = document.querySelector("ul");
     elementUl.classList.toggle("ul-opacity");
 }  
+
+
+// Ajout du smooth scrolling en jQuery
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
