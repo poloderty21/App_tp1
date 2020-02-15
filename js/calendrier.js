8f989adad1b7fb9fce698ab175df1ea7757590c9
Michael Dufresne <mic_power60@hotmail.com>
2020-02-15 18:04:13 -0500

    ajout js

--- js/calendrier.js
+++ js/calendrier.js
@@ -1,5 +1,8 @@
+let genere = false;
+
 // evt pour changer les url dynamiquement
 window.addEventListener("load",function(){
+    eventAdd();
     const calendrierTexte = document.querySelectorAll("#cours p");
     const arrayLenght = calendrierTexte.length;
     for (let i = 0; i < arrayLenght; i++) {
@@ -15,27 +18,65 @@ window.addEventListener("load",function(){
     }
 });
 
-//changer les textes de la vie étudiante
-const nodeListp =  document.querySelectorAll(".vie-etudiante p");
-const section = document.querySelector(".vie-etudiante>div");
-const element = document.createElement("div");
-const para = document.createElement("p");
-para.innerHTML = "retour";
-para.classList.add("retourColor");
-element.appendChild(para);
-nodeListp.forEach(p => {
-    p.style.textDecoration = "underline";
-    p.style.cursor = "pointer";
-    p.addEventListener("click",function(evt){
-        //console.log(evt.target);
-        section.appendChild(element);
-        const data = evt.target.getAttribute("data");
-        changerElement(data);
-    });
-});
 
-// Changer les éléments p 
+// Ajouter les éléments nécessaire
+
+creationRetour = () =>{
+    const section = document.querySelector(".vie-etudiante>div");
+    const p = document.querySelector(".vie-etudiante div div:last-child p");
+    if(!p.classList.contains("retourColor")){
+        const element = document.createElement("div");
+        const para = document.createElement("p");
+        para.innerHTML = "retour";
+        para.classList.add("retourColor");
+        element.appendChild(para);
+        section.appendChild(element);
+    }
+}
+
+
+// Ajouter un event 
+eventAdd = () =>{
+    const nodeListp =  document.querySelectorAll(".vie-etudiante p");
+    const section = document.querySelector(".vie-etudiante>div");
+    const element = document.createElement("div");
+    nodeListp.forEach(p => {
+        p.style.textDecoration = "underline";
+        p.style.cursor = "pointer";
+        p.addEventListener("click",function(evt){
+            creationRetour();
+            const data = evt.target.getAttribute("data");
+            changerElement(data);
+        });
+    });
+    
+}
+
+// Trouver le bon objet  
 
 changerElement = (data) =>{
-    console.log(data);
+    texte.forEach(element => {
+        if(data == element.section){
+            const division = element;
+            modifierTexte(division);
+        }
+    });
+}
+
+// modifier les sections avec l'objet sélectionné
+modifierTexte = (division) =>{
+    const nodeListp =  document.querySelectorAll(".p-texte");
+    const texte = division.texte;
+    console.log(texte);
+    let i = 0;
+    nodeListp.forEach(p => {
+        p.innerHTML = "";
+    });
+    nodeListp.forEach(p => {
+        p.innerHTML = texte[i];
+        if(p.innerHTML == "undefined"){
+            p.innerHTML = "";
+        }
+        i++;
+    });
 }
---
+++ js/util.js
@@ -0,0 +1,56 @@
+const texte = [
+    {
+        section:"activite",
+        texte:[
+           "arts de la scène",
+            "concours et évènements",
+            "clubs,comité et organisme",
+            "sports"
+        ],
+        link:[
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/activites/arts-culture/danse/",
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/activites/concours-evenement/concours/",
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/activites/clubs-comites-organismes-etudiants/clubs-etudiants/",
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/activites/sports/equipes-competitives-vikings/"
+        ]
+    },
+    {
+        section:"bourse",
+        texte:[
+           "programme de prêts et bourses",
+            "bourses de la fondation"
+        ],
+        link:[
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/bourses-aide-financiere/programme-de-prets-et-bourses/",
+            "https://www.cmaisonneuve.qc.ca/fondation/programmes-de-bourses-et-de-soutien/",
+        ]
+    },
+    {
+        section:"dossier",
+        texte:[
+           "aide pédagogique",
+            "annuler une session",
+            "changement de programme"
+        ],
+        link:[
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/dossier-etudiant/aide-pedagogique-individuelle/",
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/cours-absences-choix-echec/annuler-une-session/",
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/dossier-etudiant/changement-de-programme/"
+        ]
+    },
+    {
+        section:"cours",
+        texte:[
+           "absence à un cours",
+            "ajouter ou annuler un cours",
+            "choix de cours",
+            "cours complémentaires"
+        ],
+        link:[
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/cours-absences-choix-echec/absence-a-un-cours/",
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/cours-absences-choix-echec/ajouter-annuler-planifier-cours/",
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/cours-absences-choix-echec/choix-cours/",
+            "https://www.cmaisonneuve.qc.ca/vie-etudiante/cours-absences-choix-echec/cours-complementaires/"
+        ]
+    }
+];
