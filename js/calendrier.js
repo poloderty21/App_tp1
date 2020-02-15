534a45b86673be4365b0ece32f9e1fbe71e31951
Michael Dufresne <mic_power60@hotmail.com>
2020-02-15 14:00:12 -0500

    continuité de la section vie etudiante

--- js/calendrier.js
+++ js/calendrier.js
@@ -1,16 +1,26 @@
 // evt pour changer les url dynamiquement
 window.addEventListener("load",function(){
-    var calendrierTexte = document.querySelectorAll("#cours p");
-    var arrayLenght = calendrierTexte.length;
+    const calendrierTexte = document.querySelectorAll("#cours p");
+    const arrayLenght = calendrierTexte.length;
     for (let i = 0; i < arrayLenght; i++) {
         if(i == 1 || i == 6 ){
             continue;
         }
         calendrierTexte[i].style.cursor = "pointer";
         calendrierTexte[i].addEventListener("click",function(evt){
-            var urlbase = "https://tim.cmaisonneuve.qc.ca/";
-            var urlMod = urlbase + evt.target.getAttribute("data");
+            const urlbase = "https://tim.cmaisonneuve.qc.ca/";
+            const urlMod = urlbase + evt.target.getAttribute("data");
             window.open(urlMod, '_blank');
         });
     }
+});
+
+//changer les textes de la vie étudiante
+const nodeListp =  document.querySelectorAll(".vie-etudiante p");
+nodeListp.forEach(p => {
+    p.style.textDecoration = "underline";
+    p.style.cursor = "pointer";
+    p.addEventListener("click",function(evt){
+        console.log(evt.target);
+    });
 });
