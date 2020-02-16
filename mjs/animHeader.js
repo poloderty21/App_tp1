42305346ccea3700fde548d2399ceecef356d065
Michael Dufresne <mic_power60@hotmail.com>
2020-02-16 15:02:36 -0500

    animation titre site utilitaire perso done

--- mjs/animHeader.js
+++ mjs/animHeader.js
@@ -5,6 +5,17 @@ export class animHeader{
     animation(){
         this.element.style.transform = "translateY(0px)";
         this.element.style.transition = "all 2s";
+        let compteur = 0;
+        const arrayLettre = ["s","i","t","e"," ", "u","t","i","l","i","t","a","i","r","e"," ", "p","e","r","s","o"];
+        const target = document.querySelector(".global h1");
+        const arrayLenght = arrayLettre.length;
+        const anim = window.setInterval(function(){
+            target.innerHTML += `${arrayLettre[compteur]}`;
+            compteur++;
+            if(compteur ==21){
+                clearInterval(anim);
+            }
+        },200);
         this.element.style.opacity = "0.4";
         this.element.style.transform = "scale(0.6)";
         this.element.addEventListener("transitionend",function(){
