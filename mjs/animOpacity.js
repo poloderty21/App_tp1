9918008bb87a3ab94e7135c3e69c2c6d3c94a63d
Michael Dufresne <mic_power60@hotmail.com>
2020-02-16 21:40:19 -0500

    ajout d'un toop tip sur hover ul

--- mjs/animOpacity.js
+++ mjs/animOpacity.js
@@ -6,23 +6,41 @@ export class animOpacity{
         //console.log(this.element);
         this.element.style.opacity = "1";
         this.element.style.transition = "all 5s";
-        //console.log(this.element.childNodes);
+        const eleLi = document.createElement("li");
+        eleLi.style.color = "rgb(77, 77, 255)";
+        eleLi.style.position = "absolute";
+        console.log(this.element);
+        this.element.appendChild(eleLi);
         const hoverEl = this.element.childNodes;
         const array = [];
         array[0] = this.element.childNodes[1];
         array[1] = this.element.childNodes[3];
         array[2] = this.element.childNodes[5];
-        //console.log(array);
+        array[3] = this.element.childNodes[7];
         array.forEach(element => {
             element.addEventListener("mouseover",function(){
-                //console.log(this.childNodes[0]);
+                const attr = this.childNodes[0].getAttribute("href");
+                let tooltip;
+                switch (attr) {
+                    case "#vie-etudiante":
+                        tooltip = "Des liens pour aider les étudiants";
+                    break;
+                    case "#projet":
+                        tooltip = "Des projets que certain étudiants ont fait";
+                    break;
+                    case "#horaire":
+                        tooltip = "Horraire typique d'un étudiant du tim";
+                    break;
+                }
                 this.childNodes[0].style.color = "rgb(77, 77, 255)";
+                eleLi.innerHTML = tooltip;
             });
         });
         array.forEach(element => {
             element.addEventListener("mouseout",function(){
                 //console.log(this.childNodes[0]);
                 this.childNodes[0].style.color = "#fff";
+                eleLi.innerHTML = "";
             });
         });
     }
